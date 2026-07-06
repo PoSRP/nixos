10bcc9ba6f381ec2ea9c9bf729ec31b1b8d5f2cb
PoSRP <srp1134@proton.me>
2026-07-06 16:30:46 +0200

    Add CUPS (#3)

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -94,6 +94,12 @@
 
   services.fwupd.enable = true;
 
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ epson-escpr gutenprint ];
+  services.printing.cups-pdf.enable = true;
+  services.printing.cups-pdf.instances.pdf.settings.Out = "\${HOME}/cups-pdf";
+  hardware.printers.ensureDefaultPrinter = "pdf";
+
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
   xdg.portal.config.hyprland.default = [ "hyprland" "gtk" ];
@@ -191,6 +197,7 @@
     spotify
     stm32cubemx
     sublime-merge
+    system-config-printer
     (texlive.combine {
       inherit (texlive)
         scheme-medium
