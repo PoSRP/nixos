ca11f77bf3ef3774189e960ce22ad128829b7d48
SR <srp1134@proton.me>
2026-07-10 15:37:56 +0200

    Add fcitx5/mozc for JP input

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -58,6 +58,13 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+    fcitx5.waylandFrontend = true;
+  };
+
   services.xserver.enable = true;
 
   services.displayManager.autoLogin = {
