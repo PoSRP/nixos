dee829de54f052923dcca874ef19f45d8de82b29
PoSRP <srp1134@proton.me>
2026-09-08 14:08:48 +0200

    Add flatpak (#27)

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -32,6 +32,10 @@
     ];
   };
 
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
